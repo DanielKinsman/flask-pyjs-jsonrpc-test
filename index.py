641c4e333f77d165282ce14f6015384551842ab4
Daniel Kinsman <danielkinsman+git@gmail.com>
2012-07-08 19:50:59 +1000

    Initial commit

---
+++ index.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import pyjd
+from pyjamas.JSONService import JSONProxy
+from pyjamas.ui.RootPanel import RootPanel
+from pyjamas.ui.Button import Button
+from pyjamas import Window
+
+class IndexHtml(object):
+    def onModuleLoad(self):
+        self.remote = DataService()
+        self.remote.handler = self
+        
+        self.button = Button("echo", self.echo)
+        RootPanel().add(self.button)
+        
+    def echo(self):
+        self.remote.sendRequest('echo', ['hello'], self)
+    
+    def onRemoteResponse(self, response, request_info):
+        Window.alert("got a response:" + response)
+        
+    def onRemoteError(self, code, errobj, request_info):
+        Window.alert("got an error")
+
+     
+class DataService(JSONProxy):
+    def __init__(self):
+        JSONProxy.__init__(self, 'service/', ['echo'])
+    
+if __name__ == '__main__':
+    pyjd.setup("index.html")
+    app = IndexHtml()
+    app.onModuleLoad()
+    pyjd.run()
---
+++ web.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from flask import request
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return serve('index.html')
+    
+@app.route('/service/', methods=['GET', 'POST'])
+def echo():
+    if request.method == 'POST':
+        answer = json.loads(request.data)['params'][0]
+        return jsonify(result=answer)
+    
+@app.route('/<requestedfile>')
+def serve(requestedfile):
+    with file('output/' + requestedfile) as f:
+        return f.read()
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
